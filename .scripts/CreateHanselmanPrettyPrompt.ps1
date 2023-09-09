cffec47b2452e3262b29a118b49d508b1c73e351
Kerry Patrick <softwareliberationarmy@gmail.com>
2023-09-09 11:12:03 -0500

    fixing regex problem

--- .scripts/CreateHanselmanPrettyPrompt.ps1
+++ .scripts/CreateHanselmanPrettyPrompt.ps1
@@ -2,6 +2,18 @@
 # Pre-requisites: Windows Terminal installed and cross-platform PowerShell installed
 # see https://www.hanselman.com/blog/how-to-make-a-pretty-prompt-in-windows-terminal-with-powerline-nerd-fonts-cascadia-code-wsl-and-ohmyposh
 
+# create a function that can be called multiple times in this script
+function Update-Profile (
+    [Parameter(Mandatory = $true)]
+    [string]$stmt
+) {
+    $stmtExists = Select-String -Path $PROFILE -Pattern $stmt -SimpleMatch
+    if ($null -eq $stmtExists) {
+        # add the line to the $PROFILE with a line break at the end
+        Add-Content -Path $PROFILE -Value "$stmt`n"        
+    }
+}
+
 # on error stop
 $ErrorActionPreference = "Stop"
 
@@ -22,11 +34,7 @@ if (!(Test-Path -Path $PROFILE )) {
 
 # loop through the import statements and check if they exist in the $PROFILE
 foreach ($stmt in $importStmts) {
-    $stmtExists = Select-String -Path $PROFILE -Pattern $stmt
-    if ($null -eq $stmtExists) {
-        # add the line to the $PROFILE with a line break at the end
-        Add-Content -Path $PROFILE -Value "$stmt`n"        
-    }
+    Update-Profile -stmt $stmt
 }
 
 $targetOmpFile = "$env:USERPROFILE\kpatrick.omp.json"
@@ -41,10 +49,6 @@ if (!(Test-Path -Path $targetOmpFile)) {
     }
 }
 
-$ohMyPoshCommand = "oh-my-posh --init --shell pwsh --config ~\kpatrick.omp.json | Invoke-Expression"
-
 # add the oh-my-posh command to the $PROFILE
-$stmtExists = Select-String -Path $PROFILE -Pattern $ohMyPoshCommand
-if ($null -eq $stmtExists) {
-    Add-Content -Path $PROFILE -Value "$ohMyPoshCommand`n"
-}
+Update-Profile -stmt "oh-my-posh --init --shell pwsh --config ~\kpatrick.omp.json | Invoke-Expression"
+

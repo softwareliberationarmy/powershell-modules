effd2825783930c1412daf73d7c0ad9fe6833e62
kerry-patrick-il <146291855+kerry-patrick-il@users.noreply.github.com>
2024-07-21 20:39:14 -0500

    adding jest-environment-jsdom package for react testing

--- modules/New-Jest/New-Jest.psm1
+++ modules/New-Jest/New-Jest.psm1
@@ -2,7 +2,7 @@ Function New-Jest {
     # Main Body
     Add-PackageJson # call the Add-PackageJson function
 
-    npm install --save-dev jest # install jest
+    npm install --save-dev jest jest-environment-jsdom # install jest and jest-environment-jsdom
     npx jest --init # create the jest.config.js
     npm install --save-dev @babel/core @babel/preset-env babel-jest # install babel
     npm install --save-dev @types/jest @babel/preset-typescript # install jest types

e70113b216b4af8a8f1a9f36d504d5d5c62a5933
kerry-patrick-il <146291855+kerry-patrick-il@users.noreply.github.com>
2024-07-21 20:42:53 -0500

    extracting setup logic for jest

--- modules/New-Jest/New-Jest.psm1
+++ modules/New-Jest/New-Jest.psm1
@@ -1,20 +1,8 @@
 Function New-Jest {    
     # Main Body
     Add-PackageJson # call the Add-PackageJson function
-
-    npm install --save-dev jest jest-environment-jsdom # install jest and jest-environment-jsdom
-    npx jest --init # create the jest.config.js
-    npm install --save-dev @babel/core @babel/preset-env babel-jest # install babel
-    npm install --save-dev @types/jest @babel/preset-typescript # install jest types
-    "module.exports = {
-    presets: [
-            ['@babel/preset-env', {targets: {node: 'current'}}],
-            '@babel/preset-typescript',
-        ],
-    };" | Out-File -FilePath ".\babel.config.js" # create the babel.config.js
-
-    Open-VsCode # open the current directory in VS Code
-    
+    Install-Jest # npm install jest and setup the config
+    Open-VsCode # open the current directory in VS Code    
 }
 
 Export-ModuleMember -Function New-Jest

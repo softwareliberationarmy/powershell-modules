ca6a9ccae4f83b180b606c5d04ddd2a17b0008fa
kerry-patrick-il <146291855+kerry-patrick-il@users.noreply.github.com>
2024-07-29 20:01:27 -0500

    adding react testing libraries to the install-jest function

--- modules/Install-Jest/Install-Jest.psm1
+++ modules/Install-Jest/Install-Jest.psm1
@@ -1,4 +1,12 @@
 Function Install-Jest {    
+    param(
+        [Parameter(Mandatory=$false)]
+        [bool]$includeTestingLibraries = $true
+    )
+    # halt on error
+    $ErrorActionPreference = "Stop"
+    $PSNativeCommandUseErrorActionPreference = $true
+
     # Main Body
     npm install --save-dev jest jest-environment-jsdom # install jest and jest-environment-jsdom
     npx jest --init # create the jest.config.js
@@ -11,6 +19,9 @@ Function Install-Jest {
         ],
     };" | Out-File -FilePath ".\babel.config.js" # create the babel.config.js
     
+    if ($includeTestingLibraries -eq $true) {
+        npm install --save-dev @testing-library/react @testing-library/jest-dom @testing-library/user-event @testing-library/dom 
+    }   
 }
 
 Export-ModuleMember -Function Install-Jest
